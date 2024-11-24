fd9c1c526c9cc61120053b5a6b20c28ad4df3721
Pokiza <129907249+pokizadev@users.noreply.github.com>
2024-11-23 22:08:12 -0500

    Add Hooks classes

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,58 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.*;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    //import the @Before coming from io.cucumber.java
+    @Before (order = 1)
+    public void setupMethod(){
+        System.out.println("---> @Before: RUNNING BEFORE EACH SCENARIO");
+    }
+
+    //@Before (value = "@login", order = 2 )
+//    public void login_scenario_before(){
+//        System.out.println("---> @Before: RUNNING BEFORE EACH SCENARIO");
+//    }
+
+    @Before (value = "@db", order = 2 )
+    public void login_scenario_before(){
+        System.out.println("---> Database testing related setting for each scenario which @db tah");
+    }
+
+    /*
+    @After will be executed automatically after EVERY scenario in the project.
+     */
+    @After
+    public void teardownMethod(Scenario scenario){
+
+        if (scenario.isFailed()) {
+
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+        System.out.println("---> @After: RUNNING AFTER EACH SCENARIO");
+
+        BrowserUtils.sleep(2);
+        Driver.closeDriver();
+
+    }
+
+    //@BeforeStep
+    public void setupStep(){
+        System.out.println("-----> @BeforeSTEP : Running before each step!");
+    }
+
+    //@AfterStep
+    public void teardownStep(){
+        System.out.println("-----> @AfterSTEP : Running after each step!");
+    }
+
+}
+
