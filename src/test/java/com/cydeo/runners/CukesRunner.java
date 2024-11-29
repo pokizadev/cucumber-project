2738fa290e446db11a12e56e08e13aa6a1d98de4
Pokiza <129907249+pokizadev@users.noreply.github.com>
2024-11-28 21:22:04 -0500

    Add Cucumber Options to Cukes Runner

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -1,2 +1,24 @@
-package com.cydeo.runners;public class CukesRunner {
+package com.cydeo.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features", // business layer
+        glue = "com/cydeo/step_definitions", // implementation layer
+        dryRun = false, // true: it will run only feature file
+        // false: it will run both stepdef and feature file
+        tags = "@librarian",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {
+
 }
