92b205bd886464a3186e067d952d691fc08dfc2c
Pokiza <129907249+pokizadev@users.noreply.github.com>
2024-11-28 21:32:22 -0500

    add LoginPage class with WebElement mappings and POM setup

--- src/test/java/com/cydeo/pages/LoginPage.java
+++ src/test/java/com/cydeo/pages/LoginPage.java
@@ -1,2 +1,22 @@
-package com.cydeo.pages;public class LoginPage {
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement emailInput;
+
+    @FindBy(id = "inputPassword")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//button[.='Sign in']")
+    public WebElement signinBtn;
 }
