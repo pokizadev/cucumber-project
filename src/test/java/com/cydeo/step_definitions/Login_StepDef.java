b882158775b73392fcabcce9174c89b1048cc5f8
Pokiza <129907249+pokizadev@users.noreply.github.com>
2024-11-28 21:32:51 -0500

    Add Login Step Definitions

--- src/test/java/com/cydeo/step_definitions/Login_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDef.java
@@ -1,2 +1,40 @@
-package com.cydeo.step_definitions;public class Login_StepDef {
+package com.cydeo.step_definitions;
+
+import com.cydeo.pages.LoginPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class Login_StepDef {
+
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("qa2_url"));
+    }
+
+    @When("I login as a librarian")
+    public void i_login_as_a_librarian() {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.emailInput.sendKeys(ConfigurationReader.getProperty("lib27_user"));
+
+        loginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("lib27_pass"));
+
+        loginPage.signinBtn.click();
+    }
+
+    @Then ("dashboard should be displayed")
+    public void dashboard_should_be_displayed() {
+
+        BrowserUtils.sleep(3);
+
+        String expectedUrl = "dashboard";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrl.contains(expectedUrl));
+    }
 }
