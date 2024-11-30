0f8257d463ca8996447bcc9db02c58ec58a23b41
Pokiza <129907249+pokizadev@users.noreply.github.com>
2024-11-29 23:44:10 -0500

    Use dynamic login method for both scenarios

--- src/test/java/com/cydeo/step_definitions/Login_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDef.java
@@ -11,6 +11,8 @@ import org.junit.Assert;
 
 public class Login_StepDef {
 
+    LoginPage loginPage = new LoginPage();
+
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("qa2_url"));
@@ -18,13 +20,12 @@ public class Login_StepDef {
 
     @When("I login as a librarian")
     public void i_login_as_a_librarian() {
-        LoginPage loginPage = new LoginPage();
 
-        loginPage.emailInput.sendKeys(ConfigurationReader.getProperty("lib27_user"));
+//        loginPage.emailInput.sendKeys(ConfigurationReader.getProperty("lib27_user"));
+//        loginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("lib27_pass"));
+//        loginPage.signinBtn.click();
 
-        loginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("lib27_pass"));
-
-        loginPage.signinBtn.click();
+        loginPage.login(ConfigurationReader.getProperty("lib27_user"), ConfigurationReader.getProperty("lib27_pass"));
     }
 
     @Then ("dashboard should be displayed")
@@ -36,5 +37,16 @@ public class Login_StepDef {
         String actualUrl = Driver.getDriver().getCurrentUrl();
 
         Assert.assertTrue(actualUrl.contains(expectedUrl));
+    }
+
+    @When("I login as a student")
+    public void iLoginAsAStudent() {
+        loginPage.login(ConfigurationReader.getProperty("student27_user"), ConfigurationReader.getProperty("student27_pass"));
+    }
+
+    @Then("books should be displayed")
+    public void booksShouldBeDisplayed() {
+
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("books"));
     }
 }
