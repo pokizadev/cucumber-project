9e44fd1990224039fd0f60651d01348f5aed9a9f
Pokiza <129907249+pokizadev@users.noreply.github.com>
2024-11-30 22:49:42 -0500

    Add BasePage

--- src/test/java/com/cydeo/pages/BasePage.java
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -1,2 +1,27 @@
-package com.cydeo.pages;public class BasePage {
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(linkText = "Users")
+    public WebElement usersLink;
+
+    @FindBy(linkText = "Dashboard")
+    public WebElement dashboardLink;
+
+    @FindBy(linkText = "Books")
+    public WebElement booksLink;
+
+    @FindBy(css = "a#navbarDropdown>span")
+    public WebElement userInfo;
+
+
 }
